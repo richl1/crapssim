3a6525060242fe18e157d4f5f69cff7cd6778137
Richard Lorenzo <rlorenzo@protonmail.com>
2025-08-08 18:11:41 -0400

    1st add of my_strategies.py and partial HeatSeeker Class

---
+++ crapssim/strategy/my_strategies.py
@@ -0,0 +1,206 @@
+"""My Strategies"""
+
+import typing
+
+from crapssim.bet import Come, DontCome, DontPass, Field, PassLine, Place
+from crapssim.strategy.odds import (
+    DontPassOddsMultiplier,
+    OddsMultiplier,
+    PassLineOddsMultiplier,
+)
+from crapssim.strategy.single_bet import (
+    BetCome,
+    BetDontPass,
+    BetField,
+    BetPassLine,
+    BetPlace,
+    StrategyMode,
+)
+from crapssim.strategy.tools import (
+    AddIfNotBet,
+    AddIfPointOff,
+    AddIfPointOn,
+    AddIfTrue,
+    AggregateStrategy,
+    CountStrategy,
+    Player,
+    RemoveByType,
+    RemoveIfTrue,
+    Strategy,
+    Table,
+    WinProgression,
+)
+
+class HeatSeeker(Strategy):
+    """HeatSeeker Strategy Rules:
+    1. Bet not more than 6 units per shooter
+    2. Bet 1st Come bet after Pass point is set by other players
+    3. If Come/Pass points = 6/8, remove the Place bet
+    4. Bet new Come/Pass/Place (6/8) bets under these rules:
+        a. Never have move that 4 Come, Pass, and Place bets
+        b. Never have > 3 units "at_risk" if a seven rolls: at_risk = 
+                +sum of Come/Pass contract bets
+                +sum of Place bets
+                - 1 (if +1 or more for shooter)
+                - 1 (if new bet includes Come or Pass)
+        d. Prefer new 6/8 place bets to new Come/Pass bets.
+        e. Remove Place bets to meet "at_risk" rule
+    """
+    def __init__(
+        self,
+        pass_come_amount: float = 5,
+        six_eight_amount: float = 6,
+        shooter_budget: float = 6 * self.pass_come_amount,
+        bankroll_new_shooter: float = 0,
+        ):
+        
+        super().__init__()
+        self.pass_come_amount = float(pass_come_amount)
+        self.six_eight_amount = float(six_eight_amount)
+
+
+    def completed(self, player: Player) -> bool:
+        """The strategy is completed if the player has no bets on the table, and the players
+        bankroll is too low to make any of the other bets.
+
+        Parameters
+        ----------
+        player
+            The Player whose bankroll and bets to check.
+
+        Returns
+        -------
+        True if the strategy can't continue, otherwise False.
+        """
+        return (
+            len(player.bets) == 0
+            and player.bankroll < self.pass_come_amount
+            and player.bankroll < self.six_eight_amount
+        )    
+    
+    def get_pass_line_come_points(self, player: Player) -> list[int]:
+        """Get the point number (or the table point number in the case of PassLine) for any PassLine
+        or Come bets on the table.
+
+        Parameters
+        ----------
+        player
+            The player to check the bets for.
+
+        Returns
+        -------
+        A list of integers of the points for the PassLine and Come bets.
+        """
+        pass_line_come_points = []
+        for number in (6, 8, 9, 10):
+            if (
+                player.table.point.number == number
+                and PassLine(self.pass_come_amount) in player.bets
+            ):
+                pass_line_come_points.append(number)
+            elif Come(self.pass_come_amount, number) in player.bets:
+                pass_line_come_points.append(number)
+        return pass_line_come_points
+
+    def update_bets(self, player: Player, table: Table) -> None:
+        """If the player has less than 2 PassLine and Come bets, make the bet (depending on whether
+        the point is on or off.) If the point is on, place the 6 and 8 unless there is a PassLine or
+        Come bet with those then move them to the 5 or 9.
+
+        Parameters
+        ----------
+        player
+            The player to check on and make the bets for.
+        """
+        points_bet = []
+
+        for x in player.bets:
+            if isinstance(x, (PassLine, Come)):
+                points_bet.append(x.number)
+            if isinstance(x, (Place)):
+                player.remove_bet(x)        # Remove all Place bets.  Will add back later if allowed. 
+                                            # This simplifies the code by reducing the match - case conditions.
+        
+        pass_come_count = len(points_bet)
+
+        if table.new_shooter:
+            self.bankroll_new_shooter = player.bankroll
+            if player.bankroll >= 6 * self.pass_come_amount:
+                self.shooter_budget = 6 * self.pass_come_amount
+            else:
+                self.shooter_budget = player.bankroll
+            return  #No updates on new shooter 1st roll
+        
+        if (pass_come_count == 0) and (table.point.status == "Off"):
+            return #No updates - wait for a point to be established by other players
+
+        if (pass_come_count == 0) and (table.point.status == "On"):
+            #Start strategy with come and 6/8 place bets
+            BetCome(self.pass_come_amount).update_bets(player)
+            BetPlace({6: self.six_eight_amount, 8: self.six_eight_amount}, skip_point=True)
+            return 
+
+
+        match pass_come_count:
+            case 0:
+            # Add up to 3 new bets: Come/PassLine and 6 and/or 8 where not a point
+                if table.point.number == "On":
+                    BetCome(self.pass_come_amount).update_bets(player)
+                else:
+                    BetPassLine(self.pass_come_amount).update_bets(player)
+
+                if (6 not in points_bet):   #Place the 6 if not a point
+                    BetPlace({6: self.six_eight_amount, 8: self.six_eight_amount}, skip_point=True).update_bets(player)
+                if (8 not in points_bet):   #Place the 8 if not a point
+                    BetPlace({8: self.six_eight_amount}, skip_point=True).update_bets(player)
+                return
+            
+            case 1:
+                # Add up to 3 new bets: Come/PassLine and 6 and/or 8 where not a point
+                if (6 not in points_bet):   #Place the 6 if not a point
+                    BetPlace({6: self.six_eight_amount, 8: self.six_eight_amount}, skip_point=True).update_bets(player)
+                if (8 not in points_bet):   #Place the 8 if not a point
+                    BetPlace({8: self.six_eight_amount}, skip_point=True).update_bets(player)
+
+                if table.point.number == "On":
+                    BetCome(self.pass_come_amount).update_bets(player)
+                else:
+                    BetPassLine(self.pass_come_amount).update_bets(player)
+                return
+            
+            case 2: #Add up to 2 new bets
+                place_count = 0
+                if (6 not in points_bet) or (8 not in points_bet):
+                    if (6 not in points_bet):
+                        BetPlace({6: self.six_eight_amount, 8: self.six_eight_amount}, skip_point=True).update_bets(player)
+                        place_count += 1    
+                    elif (8 not in points_bet):
+                        BetPlace({8: self.six_eight_amount}, skip_point=True).update_bets(player)
+                        place_count += 1
+                    return  #prefer to Place 6 or 8 before Come/ PassLine
+                
+                if place_count <= 1:    #Add a Come/PassLine bet too if 0 or 1 Place bets made.
+                    if table.point.number == "On":  #add Come or PassLine bet
+                        BetCome(self.pass_come_amount).update_bets(player)
+                    else:
+                        BetPassLine(self.pass_come_amount).update_bets(player)
+                    return
+            
+            case 3: # Add a Come/PassLine bet -- allowed since it wins on a 7 and up to 4 points are allowed.
+                    # Adding a Place bet is not allowed because > 3 units at risk
+                if table.point.number == "On":
+                    BetCome(self.pass_come_amount).update_bets(player)
+                else:
+                    BetPassLine(self.pass_come_amount).update_bets(player)
+                return
+            
+            case 4 | 5 : # This case should never run.  TBD raise an exemption
+                return
+        
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(pass_come_amount={self.pass_come_amount}, "
+            f"six_eight_amount={self.six_eight_amount}, "
+        )
+
+   
